a00633d229e98fdd447335b2facd7f44b42c168b
ViniciusItalo <vinicius.italo@kultivi.com>
2022-05-17 16:14:01 -0300

    refactor: component header with conditions logged

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import { useRouter } from "next/router";
 
 import { LogoComponent } from "../Logo";
@@ -10,6 +11,8 @@ import styles from "./styles.module.scss";
 
 export function HeaderComponent() {
   const router = useRouter();
+  const isAuthenticated = false;
+  const haveAvatarProfile = false;
 
   return (
     <header className={styles.header}>
@@ -26,11 +29,28 @@ export function HeaderComponent() {
           ))}
         </ul>
 
-        <ButtonComponent
-          title="Entrar"
-          color="light"
-          onClick={() => router.push("/signin")}
-        />
+        {isAuthenticated ? (
+          <div className={styles.infoUser}>
+            <span>Nome do usuário</span>
+
+            <div className={styles.avatarProfile}>
+              {haveAvatarProfile && (
+                <Image
+                  src="/"
+                  alt="Foto de perfil do usuário"
+                  width={48}
+                  height={48}
+                />
+              )}
+            </div>
+          </div>
+        ) : (
+          <ButtonComponent
+            title="Entrar"
+            color="light"
+            onClick={() => router.push("/signin")}
+          />
+        )}
       </nav>
     </header>
   );
