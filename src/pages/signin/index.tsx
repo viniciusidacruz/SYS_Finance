b13474b61ef68ae7224be17d4ef450c3d70a3ce3
ViniciusItalo <vinicius.italo@kultivi.com>
2022-05-17 19:30:30 -0300

    refactor: component logo and page signin

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -1,4 +1,8 @@
+import { Fragment, useEffect } from "react";
+import Head from "next/head";
 import Image from "next/image";
+import { useRouter } from "next/router";
+import { useSession } from "next-auth/react";
 
 import { LoginComponent } from "components/Forms/Login";
 
@@ -7,22 +11,39 @@ import login from "assets/svg/login.svg";
 import styles from "./styles.module.scss";
 
 export default function SignIn() {
-  return (
-    <main className="container contentFull">
-      <section className={styles.content}>
-        <div className={styles.groupLeft}>
-          <Image
-            src={login}
-            width={550}
-            height={500}
-            alt="Imagem ilustrativa de um rapaz saindo pela porta"
-          />
-        </div>
+  const router = useRouter();
+  const session = useSession();
 
-        <div>
-          <LoginComponent />
-        </div>
-      </section>
-    </main>
+  const isAuthenticated = session.data?.user;
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      router.replace("/dashboard/graphic");
+    }
+  }, [session]);
+
+  return (
+    <Fragment>
+      <Head>
+        <title>Entrar - Desenvolvido por Vinicius Italo</title>
+      </Head>
+
+      <main className="container contentFull">
+        <section className={styles.content}>
+          <div className={styles.groupLeft}>
+            <Image
+              src={login}
+              width={550}
+              height={500}
+              alt="Imagem ilustrativa de um rapaz saindo pela porta"
+            />
+          </div>
+
+          <div>
+            <LoginComponent />
+          </div>
+        </section>
+      </main>
+    </Fragment>
   );
 }
