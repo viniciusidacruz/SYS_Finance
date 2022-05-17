3adeb8083d97ab3cc875b956b6f66898dc1fefb4
ViniciusItalo <vinicius.italo@kultivi.com>
2022-05-17 13:59:01 -0300

    feat: finish component header

---
+++ src/components/Header/data.ts
@@ -0,0 +1,5 @@
+export const dataNavigation = [
+  { title: "Inicío", path: "/", target: "_self" },
+  { title: "Sobre", path: "/about", target: "_self" },
+  { title: "Suporte", path: "/support", target: "_blank" },
+];
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,26 +1,36 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 import { LogoComponent } from "../Logo";
 import { ButtonComponent } from "../Button";
 
+import { dataNavigation } from "./data";
+
 import styles from "./styles.module.scss";
 
 export function HeaderComponent() {
+  const router = useRouter();
+
   return (
     <header className={styles.header}>
       <nav className="container">
         <LogoComponent />
 
         <ul>
-          <li>
-            <Link href="/">Inicío</Link>
-          </li>
-          <li>
-            <Link href="/sobre">Sobre</Link>
-          </li>
+          {dataNavigation.map((nav, index) => (
+            <li key={`${nav.title}-${index}`}>
+              <Link href={nav.path} passHref>
+                {nav.title}
+              </Link>
+            </li>
+          ))}
         </ul>
 
-        <ButtonComponent title="Entrar" />
+        <ButtonComponent
+          title="Entrar"
+          color="light"
+          onClick={() => router.push("/signin")}
+        />
       </nav>
     </header>
   );
