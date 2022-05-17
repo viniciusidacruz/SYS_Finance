7f3c0d6b03e13479ede82d0ce589ddc30f15878b
ViniciusItalo <vinicius.italo@kultivi.com>
2022-05-17 13:57:00 -0300

    feat: finish landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,55 @@
 import { Fragment } from "react";
 import Head from "next/head";
+import Image from "next/image";
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
+
+import { ButtonComponent } from "components/Button";
+import { TypographicComponent } from "components/Typographic";
+
+import hero from "assets/svg/hero.svg";
+
+import styles from "../styles/home.module.scss";
 
 const Home: NextPage = () => {
+  const router = useRouter();
+
   return (
     <Fragment>
       <Head>
         <title>SYS Finance | Developed by Vinicius Italo</title>
       </Head>
 
-      <main></main>
+      <main className={styles.container}>
+        <section className={styles.hero}>
+          <div>
+            <div className={styles.infoGroup}>
+              <TypographicComponent h5 title="Teste" />
+              <TypographicComponent h1 title="What is Lorem Ipsum?" primary />
+              <TypographicComponent
+                p
+                title="Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+Lorem Ipsum has been the industry's standard dummy text ever since the 
+1500s, when an unknown printer took a galley of type and scrambled it to
+ make a type specimen book"
+              />
+              <ButtonComponent
+                title="Faça sua cotação!"
+                color="primary"
+                onClick={() => router.push("/dashboard")}
+              />
+            </div>
+          </div>
+          <div>
+            <Image
+              src={hero}
+              width={550}
+              height={500}
+              alt="Uma imagem ilustrativa de um boneco colocando uma moeda dentro de um cofrinho em formato de porco"
+            />
+          </div>
+        </section>
+      </main>
     </Fragment>
   );
 };
