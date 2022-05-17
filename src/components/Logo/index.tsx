c35d152ad09b9207aa8fc8a1733935ac4e66b765
ViniciusItalo <vinicius.italo@kultivi.com>
2022-05-17 13:58:26 -0300

    feat: finish component logo

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -3,10 +3,15 @@ import Link from "next/link";
 
 import logo from "assets/svg/icon.svg";
 
+import styles from "./styles.module.scss";
+
 export function LogoComponent() {
   return (
     <Link href="/" passHref>
-      <Image src={logo} alt="Logo completo do SYS Finance" />
+      <div className={styles.container}>
+        <Image src={logo} alt="Logo completo do SYS Finance" />
+        <span>YS</span>
+      </div>
     </Link>
   );
 }
