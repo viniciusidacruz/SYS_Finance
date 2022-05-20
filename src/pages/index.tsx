ebb8b8d1c8cbc2f80a00e47b983474315acff448
ViniciusItalo <vinicius.italo@kultivi.com>
2022-05-20 03:37:15 -0300

    refactor: change route for call to action

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,14 +33,14 @@ const Home: NextPage = () => {
               <TypographicComponent
                 variant="p"
                 title="Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-Lorem Ipsum has been the industry's standard dummy text ever since the 
-1500s, when an unknown printer took a galley of type and scrambled it to
- make a type specimen book"
+                      Lorem Ipsum has been the industry's standard dummy text ever since the 
+                      1500s, when an unknown printer took a galley of type and scrambled it to
+                      make a type specimen book"
               />
               <ButtonComponent
                 title="Faça sua cotação!"
                 color="primary"
-                onClick={() => router.push("/dashboard")}
+                onClick={() => router.push("/dashboard/registerTransaction")}
               />
             </div>
           </div>
