30105acef2b1b807c764ea1fbbe82b0265f458c8
ViniciusItalo <vinicius.italo@kultivi.com>
2022-05-23 01:37:03 -0300

    refactor: change types and styles of component SelectCategory

--- src/components/SelectCategory/index.tsx
+++ src/components/SelectCategory/index.tsx
@@ -1,12 +1,16 @@
-import React from "react";
 import { IContentProps } from "./types";
+
+import styles from "./styles.module.scss";
 
 export function SelectCategoryComponent({
   options,
   handleChangeOptions,
 }: IContentProps) {
   return (
-    <select onChange={(event) => handleChangeOptions(event.target.value)}>
+    <select
+      className={styles.container}
+      onChange={(event) => handleChangeOptions(event.target.value)}
+    >
       {options.map((option, index) => (
         <option key={index} value={option.value}>
           {option.title}
